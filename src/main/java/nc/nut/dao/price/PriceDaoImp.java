f3892473b9abe877d5d9ae5333cec79eb41262eb
Ann Rysakova <rysakova.ann@gmail.com>
2017-05-10 13:15:03 +0300

    edit price dao impl

--- src/main/java/nc/nut/dao/price/PriceDaoImp.java
+++ src/main/java/nc/nut/dao/price/PriceDaoImp.java
@@ -5,6 +5,9 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Yuliya Pedash on 27.04.2017.
@@ -13,6 +16,9 @@ import javax.annotation.Resource;
 public class PriceDaoImp implements PriceDao {
     private static String SELECT_PRICE_BY_PRODUCT_AND_PLACE_SQL = "SELECT PRICES.PLACE_ID, PRICES.PRODUCT_ID, PRICES.PRICE" +
             "  FROM PRICES WHERE PRODUCT_ID = :product_id AND PLACE_ID = :place_id";
+
+    private final static String ADD_PRICE_OF_PRODUCT_BY_REGION = "INSERT INTO PRICES\n " +
+            "VALUES(:productId,:placeId,:price)";
     @Resource
     private NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -42,5 +48,19 @@ public class PriceDaoImp implements PriceDao {
         params.addValue("product_id", productId);
         params.addValue("place_id", placeId);
         return jdbcTemplate.queryForObject(SELECT_PRICE_BY_PRODUCT_AND_PLACE_SQL, params, new PriceRowMapper());
+    }
+
+    @Override
+    public boolean fillPriceOfProductByRegion(ArrayList<Price> priceByRegion) {
+        List<Map<String, Object>> batchValues = new ArrayList<>(priceByRegion.size());
+        for (Price price : priceByRegion) {
+            batchValues.add(
+                    new MapSqlParameterSource("productId", price.getProductId())
+                            .addValue("placeId", price.getPlaceId())
+                            .addValue("price", price.getPrice())
+                            .getValues());
+        }
+        int[] isInsert = jdbcTemplate.batchUpdate(ADD_PRICE_OF_PRODUCT_BY_REGION, batchValues.toArray(new Map[priceByRegion.size()]));
+        return isInsert.length != 0;
     }
 }
