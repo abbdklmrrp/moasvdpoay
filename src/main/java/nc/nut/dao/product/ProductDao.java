d349c7255f5c82d154c2d4f5d509d31c629905ce
Ann Rysakova <rysakova.ann@gmail.com>
2017-05-10 15:25:02 +0300

    edit place and product dao impl

--- src/main/java/nc/nut/dao/product/ProductDao.java
+++ src/main/java/nc/nut/dao/product/ProductDao.java
@@ -1,6 +1,7 @@
 package nc.nut.dao.product;
 
 import nc.nut.dao.interfaces.Dao;
+import nc.nut.dto.PriceByRegionDto;
 import nc.nut.dto.TariffServiceDto;
 
 import java.util.ArrayList;
@@ -148,4 +149,8 @@ public interface ProductDao extends Dao<Product> {
     List<Product> getLimitedQuantityProduct(int start, int length, String sort, String search);
 
     List<Product> getProductForResidentialCustomerWithoutPrice();
+
+    List<PriceByRegionDto> getProductPriceByRegion();
+
+
 }
