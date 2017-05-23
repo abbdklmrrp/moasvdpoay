3683d4f7200af0842d03973770b6df980c4a6198
Bantom <Antoxa_baton@mail.ru>
2017-05-23 13:57:45 +0300

    deleted psvm method.

--- src/main/java/jtelecom/security/Md5PasswordEncoder.java
+++ src/main/java/jtelecom/security/Md5PasswordEncoder.java
@@ -12,10 +12,6 @@ import org.springframework.stereotype.Service;
 public class Md5PasswordEncoder implements PasswordEncoder {
     private final static String salt = "-salt-";
 
-    public static void main(String[] args) {
-        System.out.println(new Md5PasswordEncoder().encode("pmg"));
-    }
-
     @Override
     public String encode(CharSequence rawPassword) {
         return Hashing.md5().hashString(rawPassword + salt, Charsets.UTF_8).toString();
